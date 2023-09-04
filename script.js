9b9a6f2aaaad4657610f850e3b66def3506acacb
TulioDL <tuliodlf88@gmail.com>
2023-09-04 16:13:02 -0300

    JQuery problem solved

--- script.js
+++ script.js
@@ -1,75 +1,88 @@
-const posts = [
-    {
-        id: crypto.randomUUID(),
-        titulo: 'Zombies',
-        artista: 'Lacuna Coil',
-        url: './assets/y2mate.com - Zombies.mp3',
-        descripcion:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, officiis facere eveniet accusamus totam incidunt natus quo eos aut laborum magni commodi aliquid nemo ratione, corporis rem, in porro consequuntur.',
-        likes: 0,
-        comentarios: ['arrechisimo', 'supercalifragilisticuespialidoso'],
-        publicadoPor: 'Lacuna Coil',
-        fechaPublicacion: new Date().toISOString(),
-    },
-    {
-        id: crypto.randomUUID(),
-        titulo: 'Fade to Black',
-        artista: 'Metallica',
-        url: './assets/y2mate.com - Fade to Black.mp3',
-        descripcion:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, officiis facere eveniet accusamus totam incidunt natus quo eos aut laborum magni commodi aliquid nemo ratione, corporis rem, in porro consequuntur.',
-        likes: 0,
-        comentarios: ['arrechisimo', 'supercalifragilisticuespialidoso'],
-        publicadoPor: 'Metallica',
-        fechaPublicacion: new Date().toISOString(),
-    },
-    {
-        id: crypto.randomUUID(),
-        titulo: 'Fade to Black',
-        artista: 'Metallica',
-        url: './assets/y2mate.com - Fade to Black.mp3',
-        descripcion:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, officiis facere eveniet accusamus totam incidunt natus quo eos aut laborum magni commodi aliquid nemo ratione, corporis rem, in porro consequuntur.',
-        likes: 0,
-        comentarios: ['arrechisimo', 'supercalifragilisticuespialidoso'],
-        publicadoPor: 'Metallica',
-        fechaPublicacion: new Date().toISOString(),
-    },
-    {
-        id: crypto.randomUUID(),
-        titulo: 'Fade to Black',
-        artista: 'Metallica',
-        url: './assets/y2mate.com - Fade to Black.mp3',
-        descripcion:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, officiis facere eveniet accusamus totam incidunt natus quo eos aut laborum magni commodi aliquid nemo ratione, corporis rem, in porro consequuntur.',
-        likes: 0,
-        comentarios: ['arrechisimo', 'supercalifragilisticuespialidoso'],
-        publicadoPor: 'Metallica',
-        fechaPublicacion: new Date().toISOString(),
-    },
-];
+// const posts = [
+//     {
+//         id: crypto.randomUUID(),
+//         titulo: 'Zombies',
+//         artista: 'Lacuna Coil',
+//         url: './assets/y2mate.com - Zombies.mp3',
+//         descripcion:
+//             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, officiis facere eveniet accusamus totam incidunt natus quo eos aut laborum magni commodi aliquid nemo ratione, corporis rem, in porro consequuntur.',
+//         likes: 0,
+//         comentarios: ['arrechisimo', 'supercalifragilisticuespialidoso'],
+//         publicadoPor: 'Lacuna Coil',
+//         fechaPublicacion: new Date().toISOString(),
+//     },
+//     {
+//         id: crypto.randomUUID(),
+//         titulo: 'Fade to Black',
+//         artista: 'Metallica',
+//         url: './assets/y2mate.com - Fade to Black.mp3',
+//         descripcion:
+//             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, officiis facere eveniet accusamus totam incidunt natus quo eos aut laborum magni commodi aliquid nemo ratione, corporis rem, in porro consequuntur.',
+//         likes: 0,
+//         comentarios: ['arrechisimo', 'supercalifragilisticuespialidoso'],
+//         publicadoPor: 'Metallica',
+//         fechaPublicacion: new Date().toISOString(),
+//     },
+//     {
+//         id: crypto.randomUUID(),
+//         titulo: 'Fade to Black',
+//         artista: 'Metallica',
+//         url: './assets/y2mate.com - Fade to Black.mp3',
+//         descripcion:
+//             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, officiis facere eveniet accusamus totam incidunt natus quo eos aut laborum magni commodi aliquid nemo ratione, corporis rem, in porro consequuntur.',
+//         likes: 0,
+//         comentarios: ['arrechisimo', 'supercalifragilisticuespialidoso'],
+//         publicadoPor: 'Metallica',
+//         fechaPublicacion: new Date().toISOString(),
+//     },
+//     {
+//         id: crypto.randomUUID(),
+//         titulo: 'Fade to Black',
+//         artista: 'Metallica',
+//         url: './assets/y2mate.com - Fade to Black.mp3',
+//         descripcion:
+//             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, officiis facere eveniet accusamus totam incidunt natus quo eos aut laborum magni commodi aliquid nemo ratione, corporis rem, in porro consequuntur.',
+//         likes: 0,
+//         comentarios: ['arrechisimo', 'supercalifragilisticuespialidoso'],
+//         publicadoPor: 'Metallica',
+//         fechaPublicacion: new Date().toISOString(),
+//     },
+// ];
 
-const readPosts = () => {
-    const publicaciones = posts.map(post => {
-        return `<div class="card-post">
-            <div id="reproductor-${post.id}">
-                <audio class="audio" id="audio-${post.id}" onplay='pausarResto("audio-${post.id}")' preload='auto' controls src='${post.url}'></audio>
-            </div>
-            <h1>${post.titulo}</h1>
-            <h4>${post.artista}</h4>
-            <p>${post.descripcion}</p>
-            <div class="btn-box">
-                <label id='likes-${post.id}'>${post.likes}</label>
-                <button id='btnLike-${post.id}' onclick='onLike("${post.id}")' type="button" class="btn btn-outline-primary" data-bs-toggle="button" aria-pressed="true"><i class="fa-solid fa-thumbs-up"></i></button>
-                <label>${post.comentarios.length}</label>
-                <button onclick="desplegarComentarios('${post.id}')" type="button" class="btn btn-outline-primary"><i class="fa-solid fa-comment"></i></button>
-            </div>
-            <div id="comentarios-${post.id}" hidden>
-            </div>
-        </div>`;
+// const buscarPosts = async () => {
+//     const response = await fetch('http://localhost:5000/posts');
+//     const posts = await response.json();
+//     console.log(posts);
+//     return posts
+// }
+
+// const posts = buscarPosts();
+
+const readPosts =  () => {
+    fetch('http://localhost:5000/posts').then(res => res.json()).then(posts => {
+        console.log(posts);
+        const publicaciones = posts.map(post => {
+            return `<div class="card-post">
+                <div id="reproductor-${post.id}">
+                    <audio class="audio" id="audio-${post.id}" onplay='pausarResto("audio-${post.id}")' preload='auto' controls src='${post.url}'></audio>
+                </div>
+                <h1>${post.titulo}</h1>
+                <h4>${post.artista}</h4>
+                <p>${post.descripcion}</p>
+                <div class="btn-box">
+                    <label id='likes-${post.id}'>${post.likes}</label>
+                    <button id='btnLike-${post.id}' onclick='onLike("${post.id}")' type="button" class="btn btn-outline-primary" data-bs-toggle="button" aria-pressed="true"><i class="fa-solid fa-thumbs-up"></i></button>
+                    <label>${post.comentarios.length}</label>
+                    <button onclick="desplegarComentarios('${post.id}')" type="button" class="btn btn-outline-primary"><i class="fa-solid fa-comment"></i></button>
+                </div>
+                <div id="comentarios-${post.id}" hidden>
+                </div>
+            </div>`;
+        });
+    
+        document.getElementById('posts').innerHTML = publicaciones.join('');
     });
-
-    document.getElementById('posts').innerHTML = publicaciones.join('');
+    
 };
 
 const mostrarFormulario = () => {
@@ -116,7 +129,7 @@ const crearPost = e => {
         publicadoPor: 'Metallica',
         fechaPublicacion: new Date().toISOString(),
     };
-
+    
     posts.unshift(nuevoPost);
     readPosts();
 };
